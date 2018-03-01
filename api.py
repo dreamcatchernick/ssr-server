7c1ea8812ca244fa22fff6b5e39000efa99b9881
dreamcatchernick <bingu@quarkfinance.com>
2018-03-01 10:49:32 +0800

    add update

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask import jsonify
-from wtforms import Form,StringField
+from wtforms import Form,StringField, IntegerField,HiddenField
+from datetime import datetime, timedelta
 import json
 
 app = Flask(__name__)
@@ -20,16 +21,44 @@ def getallusers():
         return jsonify(data)
 
 
+@app.route('/test')
+def test():
+        with open(usersJsonFile, 'r+') as jsonFile:
+            users = json.load(jsonFile)
+            return str(len(users))
+
 @app.route('/adduser', methods=['GET', 'POST'])
 def adduser():
     form = UserForm(request.form)
     if(request.method == 'POST'):
         user = form.user.data
+        password = 'Pass@word1'
+        expire = (datetime.now() + timedelta(days=30)).strftime("%Y-%m-%d")
         # print(user.data)
 
         with open(usersJsonFile, 'r+') as jsonFile:
             users = json.load(jsonFile)
-            users[1]['user'] = user
+            id = len(users) + 1
+            users.append(
+                {
+                    "d": 0,
+                    "enable": 1,
+                    "forbidden_port": "",
+                    "method": "aes-128-ctr",
+                    "obfs": "tls1.2_ticket_auth_compatible",
+                    "passwd": password,
+                    "port": 6667,
+                    "protocol": "auth_aes128_md5",
+                    "protocol_param": "",
+                    "speed_limit_per_con": 0,
+                    "speed_limit_per_user": 0,
+                    "transfer_enable": 900727656415232,
+                    "u": 0,
+                    "user": user,
+                    "expire": expire,
+                    "id": id
+                }
+            )
             jsonFile.seek(0)  # rewind
             json.dump(users, jsonFile)
             jsonFile.truncate()
@@ -38,8 +67,35 @@ def adduser():
     return render_template('adduser.html', form=form)
 
 
+@app.route('/updateuser/<int:id>/', methods=['GET', 'POST'])
+def updateuser(id):
+    form = UserForm(request.form)
+    if(request.method == 'POST'):
+
+        user = form.user.data
+        port = form.port.data
+        with open(usersJsonFile, 'r+') as jsonFile:
+            users = json.load(jsonFile)
+            updateUser = users[id-1]
+            updateUser['user'] = user
+            jsonFile.seek(0)  # rewind
+            json.dump(users, jsonFile)
+            jsonFile.truncate()
+            return redirect('/')
+    else:
+        with open(usersJsonFile, 'r+') as jsonFile:
+            users = json.load(jsonFile)
+            user = users[id-1]
+            form.id.data = user['id']
+            form.user.data = user['user']
+            form.port.data = user['port']
+            # print(user)
+            return render_template('updateuser.html', form=form)
+
 class UserForm(Form):
+    id = IntegerField('Id')
     user = StringField('User')
+    port = IntegerField('Port')
 
 
 if __name__ == '__main__':
